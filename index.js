cb63478521463672804fee7cdae085ad2cb240c4
gluxe97 <96441367+gluxe97@users.noreply.github.com>
2022-02-01 10:37:04 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random())+1;
+
+let num3 = 36;
+let num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max(1,3,5,10,15,20);
 
 // Write your code here
